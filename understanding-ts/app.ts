09e639703d3a5315dba3af3228c167bc92e4a8ae
Leandro Reis <leandro.reiz@gmail.com>
2022-06-19 21:01:24 +0100

    TS core types - arrays

--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -1,6 +1,14 @@
 const person = {
   name: 'Leandro',
   age: 38,
+  hobbies: ['Guitar', 'Games'],
 };
 
+let favouriteActivities: string[];
+favouriteActivities = ['Sports'];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
