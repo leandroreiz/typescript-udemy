2fb283a7dd0a2243456e883c4a252e2068cbe304
Leandro Reis <leandro.reiz@gmail.com>
2022-06-19 21:07:34 +0100

    TS core types - tuples

--- understanding-ts/app.js
+++ understanding-ts/app.js
@@ -1,8 +1,12 @@
 var person = {
     name: 'Leandro',
     age: 38,
-    hobbies: ['Guitar', 'Games']
+    hobbies: ['Guitar', 'Games'],
+    role: [2, 'author']
 };
+// person.role.push('admin'); // push works on Tuples
+// person.role[1] = 10; // Type 'number' is not assignable to type 'string'
+// person.role = [0, 'admin', 'user'] // Source has 3 element(s) but target allows only 2
 var favouriteActivities;
 favouriteActivities = ['Sports'];
 console.log(person.name);
