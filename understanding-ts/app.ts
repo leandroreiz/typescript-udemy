f39b3367b1b22227e029f3dc1b14d719d1fd2a5a
Leandro Reis <leandro.reiz@gmail.com>
2022-06-19 20:55:20 +0100

    TS core types - objects

--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -1,13 +1,6 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  const result = n1 + n2;
+const person = {
+  name: 'Leandro',
+  age: 38,
+};
 
-  if (showResult) console.log(phrase + result);
-  else return result;
-}
-
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'The result is: ';
-
-add(number1, number2, printResult, resultPhrase);
+console.log(person.name);
