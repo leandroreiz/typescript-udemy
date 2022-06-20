283b28e67089b370eff1a039a29e779452dcbade
Leandro Reis <leandro.reiz@gmail.com>
2022-06-20 21:54:38 +0100

    Type unknown

--- understanding-ts/app.js
+++ understanding-ts/app.js
@@ -1,15 +1,10 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 'Leandro';
+userInput = 5;
+// Type 'unknown' is not assignable to type 'string'
+// userName = userInput;
+// If you check for the type, TypeScript will allow this assignment
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-function printResult(num) {
-    console.log("Result: ".concat(num));
-}
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-printResult(add(5, 12));
-var combineValues;
-combineValues = add;
-console.log(combineValues(8, 8));
-addAndHandle(10, 20, printResult);
--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -1,22 +1,13 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 'Leandro';
+userInput = 5;
+
+// Type 'unknown' is not assignable to type 'string'
+// userName = userInput;
+
+// If you check for the type, TypeScript will allow this assignment
+if (typeof userInput === 'string') {
+  userName = userInput;
 }
-
-function printResult(num: number) {
-  console.log(`Result: ${num}`);
-}
-
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
-
-// Tests
-
-printResult(add(5, 12));
-
-let combineValues: (a: number, b: number) => number;
-combineValues = add;
-console.log(combineValues(8, 8));
-
-addAndHandle(10, 20, printResult);
