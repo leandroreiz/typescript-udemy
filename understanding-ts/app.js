c68bc8dcbae86653025551eb8da163ee5a30abc2
Leandro Reis <leandro.reiz@gmail.com>
2022-06-20 21:42:45 +0100

    Function types and call backs

--- understanding-ts/app.js
+++ understanding-ts/app.js
@@ -4,4 +4,12 @@ function add(n1, n2) {
 function printResult(num) {
     console.log("Result: ".concat(num));
 }
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
 printResult(add(5, 12));
+var combineValues;
+combineValues = add;
+console.log(combineValues(8, 8));
+addAndHandle(10, 20, printResult);
