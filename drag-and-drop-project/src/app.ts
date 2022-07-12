d2d077640a3c6956fe94694481b7d8654cadfac1
Leandro Reis <leandro.reiz@gmail.com>
2022-07-12 23:00:48 +0100

    Fetching user input

--- drag-and-drop-project/src/app.ts
+++ drag-and-drop-project/src/app.ts
@@ -1,3 +1,21 @@
+// ------------------------------------------------------------
+// Autobind decorator
+// ------------------------------------------------------------
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+// ------------------------------------------------------------
+// Project input class
+// ------------------------------------------------------------
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -34,14 +52,44 @@ class ProjectInput {
     this.attach();
   }
 
+  private getUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredDescription.trim().length === 0 ||
+      enteredPeople.trim().length === 0
+    ) {
+      alert('Invalid input, please try again!');
+      return;
+    }
+
+    return [enteredTitle, enteredDescription, +enteredPeople];
+  }
+
+  private clearInputs() {
+    this.titleInputElement.value = '';
+    this.descriptionInputElement.value = '';
+    this.peopleInputElement.value = '';
+  }
+
+  @Autobind
   private submitHandler(event: Event) {
     event.preventDefault();
 
-    console.log(this.titleInputElement.value);
+    const userInput = this.getUserInput();
+
+    if (Array.isArray(userInput)) {
+      const [title, description, people] = userInput;
+      console.log(title, description, people);
+      this.clearInputs();
+    }
   }
 
   private configure() {
-    this.element.addEventListener('submit', this.submitHandler.bind(this));
+    this.element.addEventListener('submit', this.submitHandler);
   }
 
   private attach() {
